6a4c6954827a5ce7f6aed0565f3b40fa47a27b53
Harpreet Kaur <harpreet.kaur9208@gmail.com>
2024-08-01 16:06:42 -0400

    added homepage to package.json

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import {ProductProvider} from './components/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ProductProvider>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </ProductProvider>
   </React.StrictMode>
 );
